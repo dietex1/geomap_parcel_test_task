a6ec5a9bc24e5e9b17b69aab675b9f697b4ba105
Yaroslav Fedia <fediayar@cvut.cz>
2026-09-24 23:56:04 +0200

    ADD: config for connecting  db

--- database.sql
+++ database.sql
@@ -10,3 +10,7 @@ CREATE TABLE IF NOT EXISTS parcels(
 );
 
 CREATE INDEX idx_parcel_geom_bbox ON parcels USING GIST (geom);
+
+INSERT INTO parcels (parcel_number, ku_nazev, area_m2, owner_info, geom) VALUES
+                                                                             ('123/1', 'Jičín', 500, 'Jan Novák', ST_GeomFromText('POLYGON((15.350 50.435, 15.351 50.435, 15.351 50.436, 15.350 50.436, 15.350 50.435))', 4326)),
+                                                                             ('124/2', 'Jičín', 750, 'Město Jičín', ST_GeomFromText('POLYGON((15.351 50.435, 15.352 50.435, 15.352 50.436, 15.351 50.436, 15.351 50.435))', 4326));
