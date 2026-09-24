6aca5b52369ccfa5aee39d5dad1ca29d2c732aa6
Yaroslav Fedia <fediayar@cvut.cz>
2026-09-24 23:58:15 +0200

    formatting

--- config.php
+++ config.php
@@ -1,14 +1,13 @@
 <?php
-$host = '127.0.0.1';
-$db   = 'maps';
-$user = 'postgres';
-$pass = 'password';
+    $host = '127.0.0.1';
+    $db   = 'maps';
+    $user = 'postgres';
+    $pass = 'password';
 
-try {
-    $pdo = new PDO("pgsql:host=$host;dbname=$db", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die(json_encode(['error' => 'Database connection failed']));
-}
-
+    try {
+        $pdo = new PDO("pgsql:host=$host;dbname=$db", $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die(json_encode(['error' => 'Database connection failed']));
+    }
 ?>
